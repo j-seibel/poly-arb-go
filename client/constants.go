555ba6873d63c9911204598b8ff74aa0b9a94ed3
j-seibel <107728907+j-seibel@users.noreply.github.com>
2025-03-06 14:16:33 -0600

    Update constants.go

--- client/constants.go
+++ client/constants.go
@@ -11,8 +11,8 @@ import (
 
 const HOST = "https://clob.polymarket.com"
 const CHAIN_ID = 137
-const PK = "3273d17583f924c65451317e658532dc62a9cc52505183eb4b65f121660e8ed1"
-const FUNDER = "0x6cd02aAfEEb049150014D3D9356613897Ce54e6C"
+const PK = os.Env("PRIVATE_KEY")
+const FUNDER = os.ENV("FUNDER")
 
 var NEG_RISK_CONTRACT_CONFIG = &ContractConfig{
 	"0xd91E80cF2E7be2e162c6513ceD06f1dD0dA35296",
@@ -33,9 +33,9 @@ var CLOB_REQUEST_STRUCTURE = []abi.Type{
 var CLOB_ENCODE_HASH = crypto.Keccak256Hash([]byte("ClobAuth(address address,string timestamp,uint256 nonce,string message)"))
 
 var API_CREDS = APICreds{
-	"2cade072-7d71-7480-0ed0-418f0de112fa",
-	"Q4qyjHxWpCwNsPKjfnsQdUE0PFGRhlVcXH-4RE4CUYk=",
-	"f64fe106f25ccb2ec95c46e4dbb0e1d7cdb442cb54f6df4a4ac6d542bda155ca",
+	os.Env("API_KEY"),
+	os.Env("API_SECRET"),
+	os.ENV("FUNDER"),
 }
 
 var POLYMARKET_SIGNER = Signer{
